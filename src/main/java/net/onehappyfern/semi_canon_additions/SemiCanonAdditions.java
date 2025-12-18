2bf1818b49ecf12077e39a8f0a3e7897311f7e1e
OneHappyFern <bowgarrian2@gmail.com>
2025-12-17 20:41:39 -0700

    added ruby

--- src/main/java/net/onehappyfern/semi_canon_additions/SemiCanonAdditions.java
+++ src/main/java/net/onehappyfern/semi_canon_additions/SemiCanonAdditions.java
@@ -2,6 +2,7 @@ package net.onehappyfern.semi_canon_additions;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.onehappyfern.semi_canon_additions.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class SemiCanonAdditions implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
