00695484aaa9b4b6052369a0875c6fff88ccda76
OneHappyFern <bowgarrian2@gmail.com>
2025-12-17 20:21:20 -0700

    Initial commit

---
+++ src/main/java/net/onehappyfern/semi_canon_additions/SemiCanonAdditions.java
@@ -0,0 +1,16 @@
+package net.onehappyfern.semi_canon_additions;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SemiCanonAdditions implements ModInitializer {
+	public static final String MOD_ID = "semi_canon_additions";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/onehappyfern/semi_canon_additions/SemiCanonAdditionsClient.java
@@ -0,0 +1,10 @@
+package net.onehappyfern.semi_canon_additions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SemiCanonAdditionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
